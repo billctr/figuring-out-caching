868ad9a3c4e328288e06cee95d64d1fa07585cd8
Gabriel Castro <gabriel.castro@hqrevenue.com>
2022-07-21 10:50:05 +0200

    Add routing and navigation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
-import("./js/functions").then(({ sayHello, appendImage }) => {
-  sayHello();
+function handleRedirect(url) {
+  window.location.href = url;
+}
 
-  let image;
-  if (process.env.IMAGE_2) {
-    image = require("./assets/photo2.png");
-  } else {
-    image = require("./assets/photo1.webp");
+function navigating() {
+  const navButtons = document.querySelectorAll("button[data-route]");
+
+  navButtons.forEach((el) => {
+    el.onclick = () => {
+      handleRedirect(el.dataset.route);
+    };
+  });
+}
+
+async function routing() {
+  const route = window.location.pathname;
+
+  switch (route) {
+    case "/route":
+      const { loremIpsum } = await import("./js/functions");
+
+      loremIpsum();
+
+      break;
+    default:
+      const { sayHello, appendImage } = await import("./js/functions");
+
+      sayHello();
+
+      let image;
+      if (process.env.IMAGE_2) {
+        image = require("./assets/photo2.png");
+      } else {
+        image = require("./assets/photo1.webp");
+      }
+      appendImage(image);
+
+      break;
   }
-  appendImage(image);
-});
+}
+
+routing();
+navigating();
--- src/js/functions.js
+++ src/js/functions.js
@@ -1,4 +1,4 @@
-export function sayHello(isReversed) {
+export function sayHello() {
   const message = "Hello world!";
   const reversedMessage = message.split("").reverse().join("");
 
@@ -11,4 +11,16 @@ export function appendImage(image) {
   imgEl.style.width = "150px";
 
   document.body.append(imgEl);
+}
+
+export function loremIpsum() {
+  const text =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam posuere, lacus sed ultrices laoreet, arcu odio mattis risus, sit amet bibendum tortor magna sollicitudin sem. Donec elit sem, convallis scelerisque diam ut, luctus hendrerit purus. Suspendisse tempor nisi vitae neque suscipit iaculis. Phasellus sit amet nunc egestas, venenatis massa quis, lacinia justo. Aliquam ultrices hendrerit leo, non tincidunt diam tempor lobortis. Curabitur pharetra in tellus eu pulvinar. Integer quis nibh iaculis, ultrices leo ac, feugiat erat. Cras at congue ligula, non consequat enim.";
+  const reversedText = text.split("").reverse().join("");
+
+  const div = document.createElement("div");
+  div.classList.add("lorem-ipsum");
+  div.innerText = process.env.REVERSED_WELCOME ? reversedText : text;
+
+  document.body.append(div);
 }
